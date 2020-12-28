2c94b0d75ca2344aa3a968e6f0d3ab74898bf1af
chetas11 <cbirmole@gmail.com>
2020-12-28 22:12:59 +0530

    added modal

--- src/app.js
+++ src/app.js
@@ -3,15 +3,32 @@ import ReactDOM from "react-dom"
 import InputField from "./components/Searchbar"
 import Card from "./components/card"
 import AllReceipes from "./data/receipes"
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 
 
-
-
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const App = ()=>{
 
     const [receipes, setNewList] = useState(localStorage.getItem("receipes")?JSON.parse(localStorage.getItem("receipes")):AllReceipes);
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
 
     useEffect(() =>{
         localStorage.setItem("receipes", JSON.stringify(receipes))
@@ -22,6 +39,9 @@ const App = ()=>{
         NewRecipeList.splice(ClickedTasksIndex, 1)
         setNewList(NewRecipeList)
     }
+    const handleClose = () => {
+        setOpen(false);
+    };
 
 return(
     <div>
@@ -32,12 +52,33 @@ return(
                 const onDeleteClick = ()=>{
                     deleteItem(tabIndex)
                 }
-                const checkReceipe = ()=>{
-                    getReceipe(tabIndex)
-                }
+                const handleOpen = () => {
+                setOpen(true);
+                Title = item.title,
+                Receipe = item.receipe
+                };
                 return(
                 <div className="col-lg-4 col-md-4 col-sm-6">
-                <Card src={item.src} title={item.title} desc={item.desc} key={tabIndex} checkReceipe={checkReceipe} onDeleteClick = {onDeleteClick} />
+                <Card src={item.src} title={item.title} desc={item.desc} key={tabIndex} checkReceipe={handleOpen} onDeleteClick = {onDeleteClick} />
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    className={classes.modal}
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                    timeout: 500,
+                    }}
+                >
+                    <Fade in={open}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">{Title}</h2>
+                        <p id="transition-modal-description">{Receipe}</p>
+                    </div>
+                    </Fade>
+                </Modal>
                 </div>
                 )
             })}
